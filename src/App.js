654160e8b6c59a6fa1f36a0ede4aa5f5098f9a6d
SAHIL PRASAD <39877110+sahilp007@users.noreply.github.com>
2020-12-21 13:26:57 +0530

    Final Edits

--- src/App.js
+++ src/App.js
@@ -1,83 +1,85 @@
-import React, { useState } from "react";
+import {useState} from "react";
 import "./styles.css";
 
 var inputStyles = {
-  marginTop: "1rem",
-  padding: "1rem",
-  width: "80%",
-  border: "2px solid",
-  borderRadius: "11px",
-  fontSize: "larger",
-  textAlign: "center"
+	marginTop: "1rem",
+	padding: "1rem",
+	width: "80%",
+	border: "2px solid",
+	borderRadius: "11px",
+	fontSize: "larger",
+	textAlign: "center"
 };
 
 var spanStyles = {
-  padding: "0 2rem",
-  cursor: "pointer"
+	padding: "0 2rem",
+	cursor: "pointer"
 };
 
 var emojiDict = {
-  "🙆‍♀️‍": "Gesturing NO",
-  "🧏‍♂️": "Can't Hear",
-  "🙇‍♀️": "Bowing",
-  "🤦‍♂️": "Facepalm : Disappointed",
-  "🤳": "Selfie",
-  "🤙": "Call me hand",
-  "👋": "Waveing Hand",
+	"🙆‍♀️‍": "Gesturing NO",
+	"🧏‍♂️": "Can't Hear",
+	"🙇‍♀️": "Bowing",
+	"🤦‍♂️": "Facepalm : Disappointed",
+	"🤳": "Selfie",
+	"🤙": "Call me hand",
+	"👋": "Waveing Hand",
 };
 
 // get list of emojis from the dict
 var emojiList = Object.keys(emojiDict);
 
 export default function App() {
-  const [emojiMeaning, setEmojiMeaning] = useState("");
+	const [emojiMeaning, setEmojiMeaning] = useState("");
 
-  function inputEventHandler(event) {
-    var inputEmoji = event.target.value;
+	function inputEventHandler(event) {
+		var inputEmoji = event.target.value;
 
-    // if text is cleared, reset the emojiMeaning to empty string
-    if (inputEmoji === "") {
-      setEmojiMeaning("");
-    } else {
-      if (inputEmoji in emojiDict) {
-        setEmojiMeaning(emojiDict[inputEmoji]);
-      } else {
-        setEmojiMeaning("Sorry, this emoji is not in the database.");
-      }
-    }
-  }
+		// if text is cleared, reset the emojiMeaning to empty string
+		if (inputEmoji === "") {
+			setEmojiMeaning("");
+		}
+		else {
+			if (inputEmoji in emojiDict) {
+				setEmojiMeaning(emojiDict[inputEmoji]);
+			}
+			else {
+				setEmojiMeaning("Sorry, this emoji is not in the database.");
+			}
+		}
+	}
 
-  function emojiClickHandler(item) {
-    var emojiClicked = item;
-    var meaning = emojiDict[emojiClicked];
-    setEmojiMeaning(meaning);
-  }
+	function emojiClickHandler(item) {
+		var emojiClicked = item;
+		var meaning = emojiDict[emojiClicked];
+		setEmojiMeaning(meaning);
+	}
 
-  return (
-    <div className="App">
-      <h1>Animals Emoji</h1>
+	return (
+		<div className="App">
+			<h1>People's Emojis</h1>
 
-      <input
-        placeholder="Enter some animal emoji"
-        style={inputStyles}
-        onChange={inputEventHandler}
-      />
+			<input
+				placeholder="Enter some animal emoji"
+				style={inputStyles}
+				onChange={inputEventHandler}
+			/>
 
-      <div className="showMeaning">{emojiMeaning}</div>
+			<div className="showMeaning">{emojiMeaning}</div>
 
-      <div className="emojiContainer">
-        {emojiList.map((item) => {
-          return (
-            <span
-              key={item}
-              style={spanStyles}
-              onClick={() => emojiClickHandler(item)}
-            >
-              {item}
-            </span>
-          );
-        })}
-      </div>
-    </div>
-  );
+			<div className="emojiContainer">
+				{emojiList.map((item) => {
+					return (
+						<span
+							key={item}
+							style={spanStyles}
+							onClick={() => emojiClickHandler(item)}
+						>
+							{item}
+						</span>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
